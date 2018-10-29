d0dfbe8733efd67a2b2ab309ba190d6a9deab2b5
julian <julian21olarte@gmail.com>
2018-10-28 21:17:43 -0500

    add environment data.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    WebAuthConfig: {
+      clientID: 'ijlsg7NpDCuwZuOjXAmkzRzxyh9jgsRH',
+      domain: 'palito-1.auth0.com',
+      scope: 'openid user_metadata',
+      responseType: 'token'
+      // scope: 'openid profile email'
+    },
+    connection: 'Username-Password-Authentication'
+  }
 };
